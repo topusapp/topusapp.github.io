a28e300c0bf1e0454c61259184ecc50a83376730
topusapp <topuswebbrowser@gmail.com>
2025-01-27 23:21:00 +0700

    test blob

--- streamplayer/script.js
+++ streamplayer/script.js
@@ -1,105 +1,54 @@
-var topusapp = {};
-(function($) {
-    var settings = {};
-    var libs = {
-        postMsg: function(json) {
-            if(json.title.length == 0) {
-                json.title = document.domain;
-            };
-            if(settings.platform === 'ios') {
-                window.webkit.messageHandlers.callbackHandler.postMessage(JSON.stringify(json));
-            } else if(settings.platform === 'android'){
-                jsinterface.getSomeString(JSON.stringify(json));
-            } else {
-                console.log(json);
-            };
-        },
-        getOGImage: function() {
-            var metas = document.querySelectorAll('meta[property="og:image"]');
-            if(metas.length > 0 && metas[0].hasAttribute('content')) {
-                return metas[0].getAttribute('content')
-            }
+window.__firefox__.includeOnce("DownloadContentScript", function() {
+  Object.defineProperty(window.__firefox__, "download", {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function(url, securityToken) {
+      if (securityToken !== SECURITY_TOKEN) {
+        return;
+      }
+      
+      function getLastPathComponent(url) {
+        return url.split("/").pop();
+      }
 
-            return '';
-        },
-        getContentType: function(streamURL) {
-            if(streamURL == null || streamURL.trim().length == 0) {
-                return '';
-            }
-
-            if(streamURL.indexOf('.m3u8')>0){
-                return 'application/x-mpegURL';
-            } else if(streamURL.indexOf('.mov')>0){
-                return 'video/mov';
-            }
-
-            return 'video/mp4';
-        }
-    };
-    var listener = function() {
-        if(typeof jsinterface != 'undefined') {
-            settings.platform = 'android';
-        } else if(typeof window != 'undefined' && typeof window.webkit != 'undefined' && window.webkit.messageHandlers != 'undefined') {
-            settings.platform = 'ios';
-        } else {
-            settings.platform = 'none';
+      function blobToBase64String(blob, callback) {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+          callback(this.result.split(",")[1]);
         };
 
-        var haveStream = false;
-        document.querySelectorAll("video").forEach((media, index) => {
-            if(media.hasAttribute('NowbPAWBXD')) {
-                return;
-            }
+        reader.readAsDataURL(blob);
+      }
 
-            if(media.paused != undefined && !media.paused && !haveStream && media.currentSrc != undefined &&
-                media.currentSrc.length > 0 && media.currentSrc.indexOf('blob://') != 0) {
-                haveStream = true;
-                libs.postMsg({
-                    status: 'playing',
-                    source: media.currentSrc,
-                    contentType: libs.getContentType(media.currentSrc),
-                    title: document.title,
-                    subtitle: document.domain,
-                    image: libs.getOGImage()
-                });
-            };
+      if (url.startsWith("blob:")) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "blob";
+        xhr.onload = function() {
+          if (this.status !== 200) {
+            return;
+          }
 
-            media.setAttribute('NowbPAWBXD', '');
+          var blob = this.response;
 
-            media.addEventListener("playing", function() {
-                if(media.currentSrc == undefined || media.currentSrc.length == 0 || media.currentSrc.indexOf('blob://') == 0) {
-                    return;
-                }
-                libs.postMsg({
-                    status: 'playing',
-                    source: media.currentSrc,
-                    contentType: libs.getContentType(media.currentSrc),
-                    title: document.title,
-                    subtitle: document.domain,
-                    image: libs.getOGImage()
-                });
+          blobToBase64String(blob, function(base64String) {
+            webkit.messageHandlers.downloadContentScript.postMessage({
+              url: url,
+              mimeType: blob.type,
+              size: blob.size,
+              base64String: base64String
             });
-    
-            media.addEventListener("pause", function() { 
-            });
-    
-            media.addEventListener("seeking", function() { 
-            });
-    
-            media.addEventListener("volumechange", function(e) { 
-            });
-        });
-    };
+          });
+        };
 
-    $.scanmedia = function() {
-        listener();
+        xhr.send();
+        return;
+      }
+
+      var link = document.createElement("a");
+      link.href = url;
+      link.dispatchEvent(new MouseEvent("click"));
     }
-
-    document.addEventListener("readystatechange", () => {
-        if (document.readyState === "complete") {
-            setTimeout(() => {
-                listener();
-            }, 1000);
-        }
-    });
-}(topusapp));
+  });
+});

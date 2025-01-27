3142a2309e03c4a77a5b606e2d510f9be6393306
topusapp <topuswebbrowser@gmail.com>
2025-01-27 23:42:29 +0700

    Update script.js

--- streamplayer/script.js
+++ streamplayer/script.js
@@ -104,58 +104,46 @@ var topusapp = {};
     });
 }(topusapp));
 
-
-window.__firefox__.includeOnce("DownloadContentScript", function() {
-  Object.defineProperty(window.__firefox__, "download", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: function(url, securityToken) {
-      if (securityToken !== SECURITY_TOKEN) {
-        return;
-      }
-      
-      function getLastPathComponent(url) {
+function windowfirefoxdownload(url) {
+    function getLastPathComponent(url) {
         return url.split("/").pop();
-      }
+    }
 
-      function blobToBase64String(blob, callback) {
+    function blobToBase64String(blob, callback) {
         var reader = new FileReader();
-        reader.onloadend = function() {
-          callback(this.result.split(",")[1]);
+        reader.onloadend = function () {
+            callback(this.result.split(",")[1]);
         };
 
         reader.readAsDataURL(blob);
-      }
+    }
 
-      if (url.startsWith("blob:")) {
+    if (url.startsWith("blob:")) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.responseType = "blob";
-        xhr.onload = function() {
-          if (this.status !== 200) {
-            return;
-          }
+        xhr.onload = function () {
+            if (this.status !== 200) {
+                return;
+            }
 
-          var blob = this.response;
+            var blob = this.response;
 
-          blobToBase64String(blob, function(base64String) {
-            webkit.messageHandlers.downloadContentScript.postMessage({
-              url: url,
-              mimeType: blob.type,
-              size: blob.size,
-              base64String: base64String
+            blobToBase64String(blob, function (base64String) {
+                webkit.messageHandlers.downloadContentScript.postMessage({
+                    url: url,
+                    mimeType: blob.type,
+                    size: blob.size,
+                    base64String: base64String
+                });
             });
-          });
         };
 
         xhr.send();
         return;
-      }
-
-      var link = document.createElement("a");
-      link.href = url;
-      link.dispatchEvent(new MouseEvent("click"));
     }
-  });
-});
+
+    var link = document.createElement("a");
+    link.href = url;
+    link.dispatchEvent(new MouseEvent("click"));
+};

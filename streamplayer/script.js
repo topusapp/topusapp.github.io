6b57b0d247f6427d73e1f4194b58600421ea0064
topusapp <topuswebbrowser@gmail.com>
2025-02-24 23:23:44 +0700

    Add files via upload

--- streamplayer/script.js
+++ streamplayer/script.js
@@ -40,12 +40,12 @@ var topusapp = {};
                 return '';
             }
             for(var i=0;i<ytInitialPlayerResponse.streamingData.adaptiveFormats.length;i++){
-                var item = ytInitialPlayerResponse.streamingData[i];
-                if(item.adaptiveFormats.mimeType.indexOf('video/mp4') >= 0 && [137,136,135,134,133].includes(item.adaptiveFormats.itag)){
-                    if(item.adaptiveFormats.hasOwnProperty('url')) {
-                        return item.adaptiveFormats.url;
-                    } else if(item.hasOwnProperty('hlsManifestUrl')) {
-                        return item.hlsManifestUrl;
+                var item = ytInitialPlayerResponse.streamingData.adaptiveFormats[i];
+                if(item.mimeType.indexOf('video/mp4') >= 0 && [137,136,135,134,133].includes(item.itag)){
+                    if(item.hasOwnProperty('url')) {
+                        return item.url;
+                    } else if(ytInitialPlayerResponse.streamingData.hasOwnProperty('hlsManifestUrl')) {
+                        return ytInitialPlayerResponse.streamingData.hlsManifestUrl;
                     }
                 }
             }
@@ -108,7 +108,7 @@ var topusapp = {};
                 if(streamURL == undefined || streamURL.length == 0 || streamURL.indexOf('blob:') == 0) {
                     return;
                 }
-
+                
                 libs.postMsg({
                     status: 'playing',
                     source: streamURL,
